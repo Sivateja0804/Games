2cd7c5f242f360ce7260112a21ae95043c6eed37
sivateja_k <siva.teja.st@gmail.com>
2021-03-31 01:58:42 -0400

    game_bot_automaticplay file added where the program play with random selection

--- 2048/main.py
+++ 2048/main.py
@@ -13,27 +13,36 @@ def instructions():
     print(" Click d to move right")
     print(" click a to move left")
 
-if __name__ == "__main__":
+def play_game(board,ins):
+    if ins == "w":
+        board = lg.move_up(board)
+    elif ins == "s":
+        board = lg.move_down(board)
+    elif ins == "a":
+        board = lg.move_left(board)
+    elif ins == "d":
+        board = lg.move_right(board)
+    else:
+        print("Please enter the correct command")
+        return board,"continue"
+    return board,""
+
+
+def main():
     board=lg.get_grid()
     instructions()
     while True:
         print_board(board)
         print("Enter Your Command :")
-        ins=input()
-        if ins=="w":
-            board=lg.move_up(board)
-        elif ins=="s":
-            board=lg.move_down(board)
-        elif ins=="a":
-            board=lg.move_left(board)
-        elif ins=="d":
-            board=lg.move_right(board)
-        else:
-            print("Please enter the correct command")
+        ins = input()
+        board,key=play_game(board,ins)
+        if key=="continue":
             continue
-        status=lg.check_game_status(board)
-        if status=="GAME NOT OVER":
-            board=lg.generate_random_2(board)
+        status = lg.check_game_status(board)
+        if status == "GAME NOT OVER":
+            board = lg.generate_random_2(board)
         else:
             print(status)
             break
+
+#main()
