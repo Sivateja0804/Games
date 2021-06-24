833103a082c771de994fc25e5087b206780e82a6
Sivateja0804 <48017108+Sivateja0804@users.noreply.github.com>
2021-06-23 23:50:21 -0400

    Add files via upload
    Adding setup.py file

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup,find_packages
+setup(
+    name='Games',    # This is the name of your PyPI-package.
+    version='0.1.7',
+    packages=find_packages(),# Update the version number for new releases
+    entry_points={"console_scripts": ["games=Games.play_games:main"]}                 # The name of your script, and also the command you'll be using for calling it
+)
