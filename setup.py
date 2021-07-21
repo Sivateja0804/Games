42698c263a08162136d86d753a67cc5bd20d6827
Siva <siva.teja.st@gmail.com>
2021-07-20 20:04:19 -0400

    name change in setup.py file

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup,find_packages
 setup(
-    name='Games',
+    name='fun-games-0.2',
     version='0.2',
     packages=find_packages(),
     entry_points={"console_scripts": ["games=src.play_games:main"]}
